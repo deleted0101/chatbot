64fa7c4694ef75eccbcc2568741b38de862ee86e
Nikosane <niteshkotian3@gmai.com>
2023-10-12 23:27:54 +0530

    training

--- chatbot.py
+++ chatbot.py
@@ -64,8 +64,10 @@ net = tflearn.input_data(shape=[None,len(training[0])])
 net = tflearn.fully_connected(net , 8)
 net = tflearn.fully_connected(net , 8)
 net = tflearn.fully_connected(net , len(output[0]), activation = "softmax")
-net = tflearn.DNN(net)
+model = tflearn.DNN(net)
 
+model.fit(training,output, n_epoch= 1000, show_metric=True)
+model.save("model.tflearn")
 
 
 
