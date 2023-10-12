405dffd46ea2f81170f81d34f5e15e6375e3b068
Nikosane <niteshkotian3@gmai.com>
2023-10-12 23:02:11 +0530

    model

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,11 @@
 import nltk
+import tflearn
 from nltk.stem.lancaster import LancasterStemmer
 
 stemmer = LancasterStemmer()
 
 import numpy
 import json
-#import tflearn
 import tensorflow
 import random
 
@@ -16,16 +16,60 @@ print(data["intents"])
 
 words = []
 labels = []
-docs = []
-
+docs_x = []
+docs_y = []
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(wrds)
+        docs_y.append(intent["tags"])
 
     if intent["tags"] not in labels:
         labels.append(intent["tags"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None,len(training[0])])
+net = tflearn.fully_connected(net , 8)
+net = tflearn.fully_connected(net , 8)
+net = tflearn.fully_connected(net , len(output[0]), activation = "softmax")
+net = tflearn.DNN(net)
+
+
+
+
+
 
 
 
