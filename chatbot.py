430c3782c217b1da1b97de54fb1ec75c82904d1d
Nikosane <niteshkotian3@gmai.com>
2023-10-12 21:17:43 +0530

    creating a project

--- chatbot.py
+++ chatbot.py
@@ -5,12 +5,27 @@ stemmer = LancasterStemmer()
 
 import numpy
 import json
-import tflearn
+#import tflearn
 import tensorflow
 import random
 
-
-with open("intents.json") as file:
+with open("intents.json", encoding="utf-8") as file:
     data = json.load(file)
 
 print(data["intents"])
+
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs.append(pattern)
+
+    if intent["tags"] not in labels:
+        labels.append(intent["tags"])
+
+
+
