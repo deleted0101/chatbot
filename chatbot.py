5a8408e6baa41cf8b281735def7150962ed8b36b
Nikosane <niteshkotian3@gmai.com>
2023-10-13 14:47:25 +0530

    training and adding model

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-import nltk
+'''import nltk
 import tflearn
 from nltk.stem.lancaster import LancasterStemmer
 
@@ -67,7 +67,79 @@ net = tflearn.fully_connected(net , len(output[0]), activation = "softmax")
 model = tflearn.DNN(net)
 
 model.fit(training,output, n_epoch= 1000, show_metric=True)
-model.save("model.tflearn")
+model.save("model.tflearn")'''
+
+import nltk
+nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+import numpy
+import json
+import pickle
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+stemmer = LancasterStemmer()
+with open("intents.json", encoding="utf-8") as file:
+    data = json.load(file)
+    try:
+            with open("data.pickle", "rb") as f:
+                words, labels, training, output = pickle.load(f)
+    except:
+            words = []
+            labels = []
+            docs_x = []
+            docs_y = []
+            for intent in data["intents"]:
+                for pattern in intent["patterns"]:
+                    wrds = nltk.word_tokenize(pattern)
+                    words.extend(wrds)
+                    docs_x.append(wrds)
+                    docs_y.append(intent["tags"])
+
+                if intent["tags"] not in labels:
+                    labels.append(intent["tags"])
+
+            words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+            words = sorted(list(set(words)))
+
+            labels = sorted(labels)
+
+            training = []
+            output = []
+
+            out_empty = [0 for _ in range(len(labels))]
+
+            for x, doc in enumerate(docs_x):
+                bag = []
+
+            wrds = [stemmer.stem(w) for w in doc]
+
+            for w in words:
+                if w in wrds:
+                    bag.append(1)
+                else:
+                    bag.append(0)
+
+            output_row = out_empty[:]
+            output_row[labels.index(docs_y[x])] = 1
+
+            training.append(bag)
+            output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+
+X_train, X_test, y_train, y_test = train_test_split(training, output, test_size=0.2, random_state=42)
+
+model = keras.Sequential()
+model.add(keras.layers.Input(shape=(len(training[0]),)))
+model.add(keras.layers.Dense(8, activation='relu'))
+model.add(keras.layers.Dense(8, activation='relu'))
+model.add(keras.layers.Dense(len(output[0]), activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.save('my_model.keras')
 
 
 
