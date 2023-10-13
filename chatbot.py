40bf73305fbff7b6cfced0f3b141e36de7f7ad1c
Nikosane <niteshkotian3@gmai.com>
2023-10-13 15:03:44 +0530

    try & except

--- chatbot.py
+++ chatbot.py
@@ -80,53 +80,56 @@ from sklearn.model_selection import train_test_split
 stemmer = LancasterStemmer()
 with open("intents.json", encoding="utf-8") as file:
     data = json.load(file)
-    try:
-            with open("data.pickle", "rb") as f:
-                words, labels, training, output = pickle.load(f)
-    except:
-            words = []
-            labels = []
-            docs_x = []
-            docs_y = []
-            for intent in data["intents"]:
-                for pattern in intent["patterns"]:
-                    wrds = nltk.word_tokenize(pattern)
-                    words.extend(wrds)
-                    docs_x.append(wrds)
-                    docs_y.append(intent["tags"])
+try:
+    with open("data.pickle", "rb") as f:
+         words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tags"])
 
-                if intent["tags"] not in labels:
-                    labels.append(intent["tags"])
+        if intent["tags"] not in labels:
+            labels.append(intent["tags"])
 
-            words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-            words = sorted(list(set(words)))
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-            labels = sorted(labels)
+    labels = sorted(labels)
 
-            training = []
-            output = []
+    training = []
+    output = []
 
-            out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-            for x, doc in enumerate(docs_x):
-                bag = []
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-            wrds = [stemmer.stem(w) for w in doc]
+        wrds = [stemmer.stem(w) for w in doc]
 
-            for w in words:
-                if w in wrds:
-                    bag.append(1)
-                else:
-                    bag.append(0)
+        for w in words:
+            if w in wrds:
+               bag.append(1)
+            else:
+                bag.append(0)
 
-            output_row = out_empty[:]
-            output_row[labels.index(docs_y[x])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-            training.append(bag)
-            output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
     training = numpy.array(training)
     output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 
 X_train, X_test, y_train, y_test = train_test_split(training, output, test_size=0.2, random_state=42)
@@ -137,13 +140,8 @@ model.add(keras.layers.Dense(8, activation='relu'))
 model.add(keras.layers.Dense(8, activation='relu'))
 model.add(keras.layers.Dense(len(output[0]), activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.save('my_model.keras')
-
-
-
-
-
-
-
+try:
+    model.load('my_model.keras')
+except:
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.save('my_model.keras')
