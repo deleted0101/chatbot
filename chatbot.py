11f797a0eaaff23becb4b6f90c777efb60e5a2dd
Nikosane <niteshkotian3@gmai.com>
2023-10-14 22:36:08 +0530

    chatbot

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intent.json').read())
+
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pickle','rb'))
+model = load_model('chatbot_model.model')
+
+
+
